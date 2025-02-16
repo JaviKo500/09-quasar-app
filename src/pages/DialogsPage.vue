2df4b86b8619da1ed26189f6ac0ce01d2babcd09
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-16 16:47:08 -0500

    dialogs

--- src/pages/DialogsPage.vue
+++ src/pages/DialogsPage.vue
@@ -1,8 +1,64 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <h1>dialogs page</h1>
-  </q-page>
+  <div class="q-pa-md q-gutter-sm">
+    <p class="text-h4">Dialogs</p>
+    <q-separator />
+    <q-btn unelevated label="Alert" color="primary" @click="alert = true" />
+    <q-btn unelevated label="Confirm" color="primary" @click="confirm = true" />
+    <q-btn unelevated label="Prompt" color="primary" @click="prompt = true" />
+
+    <q-dialog v-model="alert">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="confirm" persistent>
+      <q-card>
+        <q-card-section class="row items-center">
+          <q-avatar icon="las la-network-wired" color="primary" text-color="white" />
+          <span class="q-ml-sm">You are currently not connected to any network.</span>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="Cancel" color="primary" v-close-popup />
+          <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="prompt" persistent position="bottom">
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Your address</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input dense v-model="address" autofocus @keyup.enter="prompt = false" />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn flat label="Add address" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
+  const alert = ref(false);
+  const confirm = ref(false);
+  const prompt = ref(false);
+  const address = ref('');
 </script>
