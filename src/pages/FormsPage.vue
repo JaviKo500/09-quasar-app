e5e5991e822101e7047f7dc70bf703fcbd6fe60c
JaViKo500 <javikogutierrez64@gmail.com>
2025-02-16 17:30:18 -0500

    add forms valid

--- src/pages/FormsPage.vue
+++ src/pages/FormsPage.vue
@@ -5,18 +5,23 @@
     <div class="row q-mt-md justify-center">
       <div class="col-sm-12 col-xs-12 col-md-6">
         <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
-          <q-input filled v-model="email" label="Your email *" hint="Enter your email" lazy-rules type="email"
-            :rules="[val => val && val.length > 0 || 'Email is required']" no-error-icon />
+          <q-input filled v-model="userForm.email" label="Your email *" hint="Enter your email" lazy-rules type="email"
+            :rules="[
+              val => val && val.length > 0 || 'Email is required',
+              isValidEmail,
+            ]" no-error-icon />
 
-          <q-input filled type="password" v-model="password" label="Enter password" lazy-rules :rules="[
+          <q-input filled type="password" v-model="userForm.password" label="Enter password" lazy-rules :rules="[
             val => val !== null && val !== '' || 'Please type your password',
           ]" no-error-icon />
-          <q-input filled type="password" v-model="confirmPassword" label="Repeat password" lazy-rules :rules="[
+          <q-input filled type="password" v-model="userForm.confirmPassword" label="Repeat password" lazy-rules :rules="[
             val => val !== null && val !== '' || 'Please repeat your password',
+            val => val === userForm.password || 'Passwords are not the same',
           ]" no-error-icon />
 
-          <q-toggle v-model="accept" label="I accept the license and terms" />
-
+          <q-checkbox v-model="userForm.conditions" label="I accept the license and terms" :class="[
+            userForm.errorInConditions && !userForm.conditions ? 'text-red' : '',
+          ]" />
           <div>
             <q-btn label="Submit" type="submit" color="primary" />
             <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
@@ -28,39 +33,41 @@
 </template>
 
 <script setup lang="ts">
-import { useQuasar } from 'quasar';
 import { ref } from 'vue';
 
-const $q = useQuasar()
 
-const email = ref(null)
-const password = ref(null)
-const confirmPassword = ref(null)
-const accept = ref(false)
-
+const userForm = ref({
+  email: null,
+  password: null,
+  confirmPassword: null,
+  conditions: false,
+  errorInConditions: false,
+});
 const onSubmit = () => {
-  if (accept.value !== true) {
-    $q.notify({
-      color: 'red-5',
-      textColor: 'white',
-      icon: 'las la-exclamation',
-      message: 'You need to accept the license and terms first'
-    });
-  }
-  else {
-    $q.notify({
-      color: 'green-4',
-      textColor: 'white',
-      icon: 'las la-check-circle',
-      message: 'Submitted'
-    });
+  console.log('<--------------- JK FormsPage --------------->');
+  console.log('userForm.value', userForm.value);
+  if (userForm.value.conditions !== true) {
+    console.log('<--------------- JK FormsPage --------------->');
+    console.log('not accepted');
+    userForm.value.errorInConditions = true;
+  } else {
+    console.log('<--------------- JK FormsPage --------------->');
+    console.log('accepted');
   }
 };
 
 const onReset = () => {
-  email.value = null;
-  password.value = null;
-  confirmPassword.value = null;
-  accept.value = false;
+  userForm.value = {
+    email: null,
+    password: null,
+    confirmPassword: null,
+    conditions: false,
+    errorInConditions: false,
+  }
+}
+
+const isValidEmail = (val: string) => {
+  const emailPattern = /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+  return emailPattern.test(val) || 'Email is invalid';
 }
 </script>
